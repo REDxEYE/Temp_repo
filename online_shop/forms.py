212e7f88ac663432bb04e953be6ac2d1aae3c220
REDxEYE <med45c@gmail.com>
2021-05-11 15:41:43 +0300

    Add payment method choice

--- online_shop/forms.py
+++ online_shop/forms.py
@@ -3,5 +3,7 @@ from django import forms
 
 class CheckoutForm(forms.Form):
     initials = forms.CharField(label='ФИО', max_length=260)
-    address = forms.CharField(label='Адрес', max_length=260)
+    address = forms.CharField(label='Адрес доставки', max_length=260)
     phone = forms.CharField(label='Контактный номер', max_length=260)
+    payment_method = forms.ChoiceField(label='Способ оплаты',
+                                       choices=[('card', 'Картой'), ('paper', 'Наличными'), ('online', 'Онлайн')])
