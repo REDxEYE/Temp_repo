fdeab7c3d553eefdf7b59b73acc05531edbb71dc
REDxEYE <med45c@gmail.com>
2021-05-11 15:36:52 +0300

    Add phone input field

--- online_shop/forms.py
+++ online_shop/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 
+
 class CheckoutForm(forms.Form):
     initials = forms.CharField(label='ФИО', max_length=260)
-    adress = forms.CharField(label='Адрес', max_length=260)
+    address = forms.CharField(label='Адрес', max_length=260)
+    phone = forms.CharField(label='Контактный номер', max_length=260)
